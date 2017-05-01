8f64ec76bf8638a7b321042905ff5fd4ac72e1eb
Reinoud van Zoelen <reinoud95@hotmail.com>
2017-05-01 15:30:28 +0200

    Added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {AppComponent} from './app.component';
-import {MemberCardComponent} from './member-card/member-card.component';
-import {SearchPipePipe} from '../pipes/search-pipe.pipe';
-import {FilterPipe} from '../pipes/filter.pipe';
-import {PotentialFormComponent} from './potential-form/potential-form.component';
-import {LoginFormComponent} from './login-form/login-form.component'
-import {LoginService} from './login.service'
-import {HeaderComponent} from './header/header.component'
-import {EditMemberComponent} from './edit-member/edit-member.component'
-import { routing } from "app/app.routes";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProspectDetailsComponent } from './prospect-details/prospect-details.component';
-import { SettingsComponent } from './settings/settings.component';
-import { ModalComponent } from './modal/modal.component';
-import { AddAppointmentModalComponent } from './add-appointment-modal/add-appointment-modal.component';
-import { ActionService } from "app/action-service.service";
-import { ProspectDataService } from "app/prospect-data.service";
-
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {MemberCardComponent} from "./member-card/member-card.component";
+import {SearchPipePipe} from "../pipes/search-pipe.pipe";
+import {FilterPipe} from "../pipes/filter.pipe";
+import {PotentialFormComponent} from "./potential-form/potential-form.component";
+import {LoginFormComponent} from "./login-form/login-form.component";
+import {LoginService} from "./login.service";
+import {HeaderComponent} from "./header/header.component";
+import {EditMemberComponent} from "./edit-member/edit-member.component";
+import {routing} from "app/app.routes";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ProspectDetailsComponent} from "./prospect-details/prospect-details.component";
+import {SettingsComponent} from "./settings/settings.component";
+import {ModalComponent} from "./modal/modal.component";
+import {AddAppointmentModalComponent} from "./add-appointment-modal/add-appointment-modal.component";
+import {ActionService} from "app/action-service.service";
+import {ProspectDataService} from "app/prospect-data.service";
+import {ProfessionDataService} from "app/profession-data.service";
 
 @NgModule({
   declarations: [
@@ -45,9 +45,10 @@ import { ProspectDataService } from "app/prospect-data.service";
     routing
   ],
   providers: [
-    LoginService, 
-    ActionService, 
-    ProspectDataService
+    LoginService,
+    ActionService,
+    ProspectDataService,
+    ProfessionDataService
   ],
   bootstrap: [AppComponent]
 })
