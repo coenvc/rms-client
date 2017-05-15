e2d1a75d8586b722f6626500d82cfa3f0cf6ffd9
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-05-15 14:18:02 +0200

    Dashboard refactor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,45 +6,49 @@ import { User } from "classes/user";
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css', '../../styles/buttons.css'], 
+  styleUrls: ['./dashboard.component.css', '../../styles/buttons.css'],
   providers: [ActionService]
 })
 export class DashboardComponent implements OnInit {
   ActionOverview: ActionOverview;
-  AllActionsOverview:ActionOverview; 
-  CurrentUser:User;  
-  AllActionsVisible = false; 
-  MyActionsVisible = true;  
+  CurrentUser: User;
 
-  constructor(public ActionService:ActionService) { 
-      this.CurrentUser = JSON.parse(localStorage.getItem('currentUser'));  
-      this.ActionService.getUserActionOverview(this.CurrentUser.id) 
-                        .subscribe(request => this.ActionOverview = request)
-       this.ActionService.getUserActionOverview(6) 
-                        .subscribe(request => console.log(request))     
-      this.ActionService.getAllActionsOverview() 
-                        .subscribe(request => this.AllActionsOverview = request)       
-   }  
+  constructor(public ActionService: ActionService) {
+    this.CurrentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.getUser();
+  }
 
-   showMyActions(event){ 
-      this.AllActionsVisible = false; 
-      this.MyActionsVisible = true; 
-      var myButton = document.getElementById("allAppointmentsTab"); 
-      myButton.className += " selected"  
-      var otherButton  = document.getElementById("myAppointmentsTab"); 
-      otherButton.className = "tabs"; 
-   } 
+  showMyActions(event) {
+    this.getUser();
+    var myButton = document.getElementById("allAppointmentsTab");
+    myButton.className += " selected"
+    var otherButton = document.getElementById("myAppointmentsTab");
+    otherButton.className = "tabs";
+  }
 
-   showAllActions(event){ 
-      this.AllActionsVisible = true; 
-      this.MyActionsVisible = false;
-      var myButton = document.getElementById("myAppointmentsTab"); 
-      myButton.className += " selected"  
-      var otherButton  = document.getElementById("allAppointmentsTab"); 
-      otherButton.className = "tabs";
-   }
+  showAllActions(event) {
+    this.getAll();
+    var myButton = document.getElementById("myAppointmentsTab");
+    myButton.className += " selected"
+    var otherButton = document.getElementById("allAppointmentsTab");
+    otherButton.className = "tabs";
+  }
 
   ngOnInit() {
+  }
+
+  getUser() {
+    this.ActionService.getUserActionOverview(this.CurrentUser.id)
+      .subscribe(request => {
+        this.ActionOverview = new ActionOverview(request.today, request.thisWeek, request.thisMonth, request.remainder)
+      })
+  }
+
+  getAll() {
+        this.ActionService.getAllActionsOverview()
+      .subscribe(request => {
+        this.ActionOverview = new ActionOverview(request.today, request.thisWeek, request.thisMonth, request.remainder)
+      })
   }
 
 }
